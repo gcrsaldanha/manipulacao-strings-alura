3b82315e597e2f5532826e57040de9a5fe443468
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2021-02-28 09:53:14 -0300

    aula 2

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
-url_base = url[0:19]
-print(url_base)
-
-url_parametros = url[20:36]
+# Separa base e parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+# Busca o valor de um parâmetro
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
