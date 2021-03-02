1d9d93f19ce25d55f9ae1447523918a1397c74d7
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2021-03-02 09:16:51 -0300

    aula 3

--- extrator_url.py
+++ extrator_url.py
@@ -34,6 +34,7 @@ class ExtratorURL:
         return valor
 
 
-extrator_url = ExtratorURL("bytebank/cambio")
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
