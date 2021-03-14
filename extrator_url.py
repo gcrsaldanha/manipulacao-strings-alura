4cc57d0c381ddc8b7d116cc13abfaa272e966d6c
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2021-03-14 17:45:30 -0300

    Desafio

--- extrator_url.py
+++ extrator_url.py
@@ -51,16 +51,21 @@ class ExtratorURL:
         return self.url == other.url
 
 
-url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=dolar&moedaDestino=real"
 extrator_url = ExtratorURL(url)
-extrator_url_2 = ExtratorURL(url)
 
-print("O tamanho da URL é: ", len(extrator_url))
-print("URL completa: ", extrator_url)
+### DESAFIO ###
+# Conversão de dólar para real
+VALOR_DOLAR = 5.50  # 1 dólar = 5.50 reais
+moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
+moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
+quantidade = extrator_url.get_valor_parametro("quantidade")
 
-# Verifica que duas instâncias com a mesma URL são iguais
-print("extrator_url == extrator_url_2? ", extrator_url == extrator_url_2)
-
-# Busca o valor do parâmetro quantidade
-valor_quantidade = extrator_url.get_valor_parametro("quantidade")
-print("Valor do parâmetro 'quantidade': ", valor_quantidade)
+if moeda_origem == "real" and moeda_destino == "dolar":
+    valor_conversao = int(quantidade) / VALOR_DOLAR
+    print("O valor de R$" + quantidade + " reais é igual a $" + str(valor_conversao) + " dólares.")
+elif moeda_origem == "dolar" and moeda_destino == "real":
+    valor_conversao = int(quantidade) * VALOR_DOLAR
+    print("O valor de $" + quantidade + " dólares é igual a R$" + str(valor_conversao) + " reais.")
+else:
+    print(f"Câmbio de {moeda_origem} para {moeda_destino} não está disponível.")
