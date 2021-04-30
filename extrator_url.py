9e1af5c287dfcebe8959f1f4dc0b9e27a1d63143
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2021-04-29 23:12:37 -0300

    Corrige referência à url

--- extrator_url.py
+++ extrator_url.py
@@ -17,7 +17,7 @@ class ExtratorURL:
             raise ValueError("A URL está vazia")
 
         padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
-        match = padrao_url.match(url)
+        match = padrao_url.match(self.url)
         if not match:
             raise ValueError("A URL não é válida.")
 
